1ab316f9b56865dfcb9d5fd9914df5347eb9ad03
Nick Pope <pope410211@gmail.com>
2015-06-18 16:37:23 -0400

    broken - placeholder for images not changing

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 
 
-$.getJSON
+$.getJSON('../../api/etsy/total-listing.json')
+.then(function(totalList){
+  console.log(totalList);
+  console.log('its a live');
+
+  $storeTitle = $('#storename');
+  $storeTitle.text(totalList.results[0].Shop.shop_name);
+
+  $storePic = $('a', 'img#storepic');
+  $storePic.attr('src', totalList.results[0].Images.url_75x75);
+
+});
