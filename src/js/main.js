add5d944a529c7eaa88481aef70248d56a9e46fa
Nick Pope <pope410211@gmail.com>
2015-06-19 01:47:48 -0400

    bull dogged :(

--- src/js/main.js
+++ src/js/main.js
@@ -1,42 +1,46 @@
-
-
-$.getJSON('../../api/etsy/total-listing.json')
-.then(function(totalList){
-  console.log(totalList);
-  console.log('its a live');
-
-  $storeTitle = $('#storename');
-  $storeTitle.text(totalList.results[0].Shop.shop_name);
-
-  $storePic = $('a', 'img#storepic');
-  $storePic.attr('src', totalList.results[0].Images.url_75x75);
-
-  $productTitle = $('#productinfo');
-  $productTitle.text(totalList.results[0].title)
-
-  $productCost = $('#cost');
-  $productCost.text(totalList.results[0].price)
-
-  $productCurrency = $('#money');
-  $productCurrency.text(totalList.results[0].currency_code)
-
-});
+//
+//
+// $.getJSON('../../api/etsy/total-listing.json')
+// .then(function(totalList){
+//   console.log(totalList);
+//   console.log('its a live');
+//
+//   $storeTitle = $('#storename');
+//   $storeTitle.text(totalList.results[0].Shop.shop_name);
+//
+//   $storePic = $('a', 'img#storepic');
+//   $storePic.attr('src', totalList.results[0].Images.url_75x75);
+//
+//   $productTitle = $('#productinfo');
+//   $productTitle.text(totalList.results[0].title)
+//
+//   $productCost = $('#cost');
+//   $productCost.text(totalList.results[0].price)
+//
+//   $productCurrency = $('#money');
+//   $productCurrency.text(totalList.results[0].currency_code)
+//
+// });
 
 new Vue ({
 
-  el: ".right-column",
+  el: ".overview-bullets",
 
-  data [
-    {
+  data: [
+
+    baseInfo: {body:
 
     }
+
   ],
 
-  methods: {
-    getJSON: function(get){
+  components: {
+    require '../../api/etsy/listing-variations'
+  }
 
-      this.get = '../../api/etsy/listing-variations'
-    }
+  methods: {
+
+    },
 
   },
 
